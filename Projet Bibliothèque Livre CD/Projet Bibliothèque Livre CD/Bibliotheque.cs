7da9554dd2efd78fe537cb7f5185595335cc0e44
Rubis61 <croicx2@hotmail.fr>
2014-12-08 18:31:19 +0100

    Ajout classe Bibliothèque

---
+++ Projet Bibliothèque Livre CD/Projet Bibliothèque Livre CD/Bibliotheque.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Bibliothèque_Livre_CD
+{
+    class Bibliotheque
+    {
+        public List<CD> ListCD { get; set; }
+        public List<Livre> ListLivres { get; set; }
+    }
+}
