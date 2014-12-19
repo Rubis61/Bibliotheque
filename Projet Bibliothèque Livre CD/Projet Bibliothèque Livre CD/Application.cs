f4d856e5147a8904f1b8c43d0a649156c4c025b1
Anni <pierre_henri_debris@orange.fr>
2014-12-19 11:59:41 +0100

    Modification Menu / Ajout application

---
+++ Projet Bibliothèque Livre CD/Projet Bibliothèque Livre CD/Application.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Bibliothèque_Livre_CD
+{
+
+    public static class Application
+    {
+        public static string choixUtilisateur;
+        public static string finDuProgramme;
+        static Menu menu = new Menu();
+        public static bool RunApplication()
+        {
+            do
+            {
+
+                menu.AfficherMenu();
+                choixUtilisateur = Console.ReadLine().ToString();
+                
+            }
+            while (choixUtilisateur != "1" && choixUtilisateur != "2" && choixUtilisateur != "3" && choixUtilisateur != "4" && choixUtilisateur != "5" && choixUtilisateur != "6" && choixUtilisateur != "7");
+
+            Console.Clear();
+
+            switch(choixUtilisateur)
+            {
+                case "1": menu.ListerLaBibliotheque();       break;
+                case "2": menu.AjouterUnLivre();             break;
+                case "3": menu.RamenerUnLivre();             break;
+                case "4": menu.RechercherLivreParTitre();    break;
+                case "5": menu.AjouterUnCd();                break;
+                case "6": menu.RamenerUnCd();                break;
+                case "7": menu.RechercherCDParTitre();       break;
+            }
+
+            return VoulezVousContinuez();
+        }
+
+        public static bool VoulezVousContinuez()
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Voulez vous arrêter ? o/n");
+                Console.WriteLine();
+                finDuProgramme = Console.ReadLine().ToString();
+            }while (finDuProgramme != "o" && finDuProgramme != "O" && finDuProgramme != "n" && finDuProgramme != "N");
+
+            if (finDuProgramme == "o" || finDuProgramme == "O")
+            {
+                return false;
+            }
+            else return true;
+        }
+        }
+
+    }
+}
--- Projet Bibliothèque Livre CD/Projet Bibliothèque Livre CD/Menu.cs
+++ Projet Bibliothèque Livre CD/Projet Bibliothèque Livre CD/Menu.cs
@@ -6,55 +6,31 @@ using System.Threading.Tasks;
 
 namespace Projet_Bibliothèque_Livre_CD
 {
-    public static class Menu
+    public class Menu
     {
-        public static string choixUtilisateur;
-        public static string finDuProgramme;
-        public static Bibliotheque bibliotheque = new Bibliotheque();
-        public static bool AfficherMenu()
+        public Bibliotheque bibliotheque = new Bibliotheque();
+        public void AfficherMenu()
         {
-            do
-            {
-                Console.Clear();
-
-                Console.WriteLine("Que voulez-vous faire?");
-                Console.WriteLine("1 - Lister la bibliothèque");
-                Console.WriteLine("2 - Ajouter un livre");
-                Console.WriteLine("3 - Ramener un livre");
-                Console.WriteLine("4 - Rechercher un livre par son titre");
-                Console.WriteLine("5 - Ajouter un CD");
-                Console.WriteLine("6 - Ramener un CD");
-                Console.WriteLine("7 - Rechercher un CD par son titre");
-                Console.WriteLine();
-
-                choixUtilisateur = Console.ReadLine().ToString();
-                
-            }
-            while (choixUtilisateur != "1" && choixUtilisateur != "2" && choixUtilisateur != "3" && choixUtilisateur != "4" && choixUtilisateur != "5" && choixUtilisateur != "6" && choixUtilisateur != "7");
 
             Console.Clear();
-
-            switch(choixUtilisateur)
-            {
-                case "1": ListerLaBibliotheque();       break;
-                case "2": AjouterUnLivre();             break;
-                case "3": RamenerUnLivre();             break;
-                case "4": RechercherLivreParTitre();    break;
-                case "5": AjouterUnCd();                break;
-                case "6": RamenerUnCd();                break;
-                case "7": RechercherCDParTitre();       break;
-            }
-
-            return VoulezVousContinuez();
+            Console.WriteLine("Que voulez-vous faire?");
+            Console.WriteLine("1 - Lister la bibliothèque");
+            Console.WriteLine("2 - Ajouter un livre");
+            Console.WriteLine("3 - Ramener un livre");
+            Console.WriteLine("4 - Rechercher un livre par son titre");
+            Console.WriteLine("5 - Ajouter un CD");
+            Console.WriteLine("6 - Ramener un CD");
+            Console.WriteLine("7 - Rechercher un CD par son titre");
+            Console.WriteLine();
         }
 
-        public static void ListerLaBibliotheque()
+        public void ListerLaBibliotheque()
         {
             Console.WriteLine("Bibliothèque");
             Console.ReadLine();
         }
 
-        public static void AjouterUnLivre()
+        public void AjouterUnLivre()
         {
             Console.WriteLine("Vous voulez ajouter un livre à la bibliothèque");
             Console.WriteLine();
@@ -65,60 +41,63 @@ namespace Projet_Bibliothèque_Livre_CD
             Console.WriteLine("Quel est l'auteur du livre?");
             string auteur = Console.ReadLine();
 
-            Console.WriteLine("Quel est le genre du livre?");
-
-            string genre = Console.ReadLine();
-
             Console.WriteLine("Quel est le numéro ISBN du livre?");
             string ISBN = Console.ReadLine();
 
-            string[] genreDisponible =
-            //bibliotheque.ajouterLivre(titre, ISBN, auteur, (GenreDuLivre)Enum.Parse(typeof(GenreDuLivre), genre));
-            
-            Console.WriteLine("Le livre " + titre + " de l'auteur " + auteur + " a bien était ajouté avec comme genre " + genre + " avec comme numéro ISBN " + ISBN);
+            bibliotheque.ajouterLivre(titre, ISBN, auteur, AfficherEtSaisirGenreDuLivre());
+
+            Console.WriteLine("Le livre " + titre + " de l'auteur " + auteur + " a bien était ajouté avec comme genre " + " avec comme numéro ISBN " + ISBN);
             Console.WriteLine("Appuyer sur une touche pour continuer...");
             Console.ReadLine();
         }
-        public static void RamenerUnLivre()
+        public void RamenerUnLivre()
         {
             Console.WriteLine("Ramener un livre");
             Console.ReadLine();
         }
-        public static void RechercherLivreParTitre()
+        public void RechercherLivreParTitre()
         {
             Console.WriteLine("Bibliothèque");
             Console.ReadLine();
         }
-        public static void AjouterUnCd()
+        public void AjouterUnCd()
         {
             Console.WriteLine("Bibliothèque");
             Console.ReadLine();
         }
-        public static void RamenerUnCd()
+        public void RamenerUnCd()
         {
             Console.WriteLine("Bibliothèque");
             Console.ReadLine();
         }
-        public static void RechercherCDParTitre()
+        public void RechercherCDParTitre()
         {
             Console.WriteLine("Bibliothèque");
             Console.ReadLine();
         }
-        public static bool VoulezVousContinuez()
+
+        public GenreDuLivre AfficherEtSaisirGenreDuLivre()
         {
+            Console.WriteLine("Quel est le genre du livre?");
+            Console.WriteLine();
+            Console.WriteLine("Tapez le genre de votre livre");
+            Console.WriteLine();
+
+            string saisieUtilisateur;
+            string[] genreDisponible = Livre.getGenresDisponibles();
+
             do
             {
-                Console.Clear();
-                Console.WriteLine("Voulez vous arrêter ? o/n");
-                Console.WriteLine();
-                finDuProgramme = Console.ReadLine().ToString();
-            }while (finDuProgramme != "o" && finDuProgramme != "O" && finDuProgramme != "n" && finDuProgramme != "N");
-
-            if (finDuProgramme == "o" || finDuProgramme == "O")
-            {
-                return false;
+              Console.Clear();
+              for (int i = 0; i < genreDisponible.Count(); i ++)
+              {
+                 Console.WriteLine(genreDisponible[i]);
+              }
+              saisieUtilisateur = Console.ReadLine().ToString();
             }
-            else return true;
+            while(!genreDisponible.Contains(saisieUtilisateur));
+
+            return (GenreDuLivre)Enum.Parse(typeof(GenreDuLivre), saisieUtilisateur);
         }
     }
 }
